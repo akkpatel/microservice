c30b688fdaf04c75abfbbcf43d441756e9834f53
Anand Patel <>
2018-06-05 14:35:50 -0400

    Initial microservice

---
+++ src/main/java/com/atdms/app/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.atdms.app.config.audit;
---
+++ src/main/java/com/atdms/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.atdms.app.web.rest.vm;
